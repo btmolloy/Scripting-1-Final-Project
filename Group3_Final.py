694c02d743a352e3549926e3105384d4092c74f6
Ben <92553518+btmolloy@users.noreply.github.com>
2023-04-28 13:10:33 -0400

    Everything except email and comments

--- Group3_Final.py
+++ Group3_Final.py
@@ -1,63 +1,119 @@
 #!/usr/bin/python
 import argparse as ap
+import os
+import sys
+
 import paramiko
 import re
+import getpass
 
-def fileCheck(IP_ADDRESS:str):
-    #function should return list of files that have been modified in last 3 weeks - also it should loop through them and print them out to console
-    #Im not sure what args are needed for this function so just fill them in as you go
-    host = "localhost"
+
+# function should return list of files that have been modified in last 3 weeks - also it should loop through them and print them out to console
+def fileCheck(IP_ADDRESS:str) -> list[str]:
+    masterList = []
     port = 22
-    
-     #Need to get username and password of the user to login
+    command = 'find ~ -type f -not -path \'*/\.*\' -mtime -21 -printf "%TY-%Tm-%Td\\t%k\\t%p\\n" | sort -n -k 2 | cut -f 1,3-'
+
     username = input("Enter the username of a user on the compromised machine: ")
-    password = input("Enter the password of the compromised user: ")
-    command = "find . -mtime -21 -ls"
-    
-    #Setup SSH connection and run command
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(host, port, username, password)
-    
-    stdin, stdout, stderr = ssh_client.exec_command(command)
-    
-    ftpFiles = stdout.readline.decode('utf-8').splitlines()
-    
-    #Regular Expressions for finding filename and mod date
-    filenameRegex = r" .\b*$"
-    dateRegex = r"[AMJFSN][a-z]{2} \d{1,2} "
-    compedFiles = []
-    #Get list of files to check through
-    print("Compromised Files")
-    print("File Name\t\t\tModify Date")
-    for f in ftpfiles:
-            #Create a list of the file names to add to the email
-            #Print the name and modify date of each file
-            print(re.search(filenameRegex, f) + "\t\t\t" + re.search(dateRegex, f))
-            compedFiles.append(re.search(filenameRegex, f))
-            #compedFiles should contain the path to the compromised files relative to the home directory.
-    
-    ssh_client.close()
-    
-    return(compedFiles) #Returns list of compromised files
+    masterList.append(username)
+    password = getpass.getpass("Enter the password of the compromised user: ")
+    masterList.append(password)
+
+    try:
+        #Setup SSH connection
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            ssh_client.connect(IP_ADDRESS, port, username=username, password=password)
+        except:
+            print("ERROR: Invalid Credentials")
+            exit(999)
+            ssh_client.close()
+
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+
+        for line in stdout.readlines():
+            line = line.strip()
+            masterList.append(line)
+
+        if len(masterList) < 3:
+            print("EXIT CODE: No compromised files found. There are no files that have been modified within the past 3 weeks.")
+            sys.exit(0)
+            ssh_client.close()
+
+        ssh_client.close()
+    except Exception as ex:
+        print(ex)
+    counter = 0
+    print("%-25s %10s" % ("Last Modified Date", "File Name"))
+    print("-------------------------------------------")
+    for line in masterList:
+        counter += 1
+        if counter > 2:
+            print(line)
+    print(" ")
+
+    counter = 0
+    for item in masterList:
+        if counter > 1:
+            path = item.split("/", 1)
+            masterList[counter] = "/" + path[1]
+        else:
+            masterList[counter] = item
+        counter += 1
+
+    return(masterList)
 
 def emailRecipient(senderEmail:str, recipientEmail:str, ctoBoolean):
     #function should email a "cute" email to the user and request the sender email password to send email
     #ctoBoolean will be true if -c was specified on start
     pass
 
-def downloadFiles(fileList, fileLocation):
-    #function should download files to a folder if this option was called if fileLocation is null or "" then should be downloaded to "Quarentine File" in home directory.
-    pass
+def downloadFiles(fileList, fileLocation, host, port):
+    try:
+        #Below creates the sftp connection to the client
+        sshPipe = paramiko.Transport(host, port)
+        sshPipe.connect(username=fileList[0], password=fileList[1])
+
+        sftp = paramiko.SFTPClient.from_transport(sshPipe)
+
+        #if the remote path exists continue
+        if sftp.stat(fileList[2]):
+            newFileName = "/COMPROMISED_FILE_" + (fileList[2].split("/"))[-1]
+
+            if fileLocation == "":
+                localFile = os.path.expanduser("~") + newFileName
+            else:
+                localFile = fileLocation + newFileName
+
+            try:
+                sftp.get(fileList[2], localFile)
+            except:
+                print("ERROR: The file '" + fileList[2] + "' could not be downloaded.")
+                sftp.close()
+                exit(0)
+
+            #once done copying sftp connection is closed and a message is printed regarding the action.
+            sftp.close()
+            print("File successfully downloaded")
+        #if the remote path doesnt exist, print error and end gracefully
+        else:
+            print("Error: Issue finding file on client")
+            sftp.close()
+            exit(0)
+
+    # if any uncaught errors occur, this will catch them and print the error code
+    except Exception as ex:
+        print(ex)
+
 
 def ipValidation(input):
-    #if input.matches(IP Address Regex)
-    regex = r"^(25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)(\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)){3}$"
+    regex = r"^(?!0\.0\.0\.0)(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-4]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+
     if re.match(regex, input):
         return True
     else:
         return False
-    #returns boolean value
 
 def emailValidation(input):
     regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
@@ -65,7 +121,6 @@ def emailValidation(input):
         return True
     else:
         return False
-    #return boolean value 
 
 def get_parser():
     #initial argument parser to define basic info about command
@@ -86,19 +141,33 @@ def get_parser():
 
 #below is main method to process the input
 def main():
+    downloadLocation = ""
     mpsr = get_parser()
-
     #try except is created for error handling
     try:
         parserResult = mpsr.parse_args()
-
         if emailValidation(parserResult.senderEmail):
             if emailValidation(parserResult.recipientEmail):
                 if ipValidation(parserResult.ipAddress):
-                    print("bottom")
+                    try:
+                        compFiles = fileCheck(parserResult.ipAddress)
+                        if parserResult.DirName is not None:
+                            if os.path.exists(parserResult.DirName):
+                                if os.path.isdir(parserResult.DirName):
+                                    downloadLocation = parserResult.DirName
+                                else:
+                                    print("ERROR: -d value is not a directory")
+                                    exit(0)
+                            else:
+                                print("ERROR: Inputted -d path does not exist")
+                                exit(0)
 
+                        downloadFiles(compFiles, downloadLocation, parserResult.ipAddress, 22)
 
-
+                    except Exception as ex:
+                        print(ex)
+                else:
+                    print("ERROR: Invalid IP Address, must be between 0.0.0.0 and 255.255.255.255")
             else:
                 print("ERROR: Invalid recipient email")
         else:
